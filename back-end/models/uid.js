2f067aa8915082093910783c191ee61e8991521c
Senuoy53 <lamrrani.younes@gmail.com>
2022-04-16 16:50:12 +0100

    first commit

--- back-end/models/uid.js
+++ back-end/models/uid.js
@@ -1,23 +1,21 @@
-const mongoose = require('mongoose')
-const { v1: uuidv1} = require('uuid');
-
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-   {
-       name: {
-           type: String,
-           trim: true,
-           required: true,
-           maxlength: 64
-       },
-       email: {
-           type: String,
-           trim: true,
-           required: true,
-           unique: true
-       },
-   },
-   { timestamps: true }
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 64,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model("Uuid", userSchema)
+module.exports = mongoose.model("Uuid", userSchema);
